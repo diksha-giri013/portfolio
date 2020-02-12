4a3a367e3a76d35763f47a9551ac0a67ccdd81f9
diksha-giri013 <diksha.giri013@gmail.com>
2020-02-12 22:10:06 +0530

    Updates

---
+++ precache-manifest.56431c3350566bc33f4f69607abbcfee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb05718b6084b4fbb6ac89fe71c3263",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "89210ba400327aa5080e",
+    "url": "/portfolio/static/css/main.73bf4f48.chunk.css"
+  },
+  {
+    "revision": "ff326d7fcf103050c9d7",
+    "url": "/portfolio/static/js/2.dcff9955.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.dcff9955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89210ba400327aa5080e",
+    "url": "/portfolio/static/js/main.9a0b314a.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
